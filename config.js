4fcf802713b903d22cb19850190994a0e5baf87d
namvu2101 <71799963+namvu2101@users.noreply.github.com>
2025-07-04 17:08:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    title: 'Anh giờ thì vẫn một mình nếu em cũng thế thì mình đi chơi 🥰',
     desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 8 tỉ người mà sao em lại chọn anh <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà 🥰',
     messDesc: 'Tối nay, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://facebook.com/vpn2101' //link mess của các bạn. VD: /dinhkaito
